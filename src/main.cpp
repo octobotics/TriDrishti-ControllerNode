26958939d0c149546be0c3eaf3a8097e2e23013a
octopriyanshu123 <octopriyanshu123>
2026-09-01 13:02:25 +0000

    update i2wManager

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <atomic>
+#include <chrono>
+#include <csignal>
+#include <iostream>
+#include <thread>
+
+#include "robot/robot.hpp"
+
+namespace
+{
+std::atomic<bool> shutdownRequested{false};
+
+void signalHandler(int)
+{
+    shutdownRequested.store(true, std::memory_order_relaxed);
+}
+} // namespace
+
+int main()
+{
+    std::signal(SIGINT, signalHandler);
+
+    std::cout << "Starting Robot..." << std::endl;
+
+    Robot::Robot& robot = Robot::Robot::instance();
+    robot.start();
+   // namespace
+    while (!shutdownRequested.load(std::memory_order_relaxed))
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+    }
+
+    std::cout << "\nStopping Robot..." << std::endl;
+
+    robot.stop();
+
+    return EXIT_SUCCESS;
+}
